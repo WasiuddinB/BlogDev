b989e93008cb51c9d493a8bacd4e3a4763cd8fe1
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2023-11-10 20:32:19 +0600

    Done with the full Interface

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -13,9 +13,9 @@ const Write = () => {
 
   return (
     <div className={styles.container}>
-        <input type='text' placeholder='Title' />
+        <input type='text' placeholder='Title' className={styles.input} />
         <div className={styles.editor}>
-            <button className={styles.button}>
+            <button className={styles.button} onClick={()=> setOpen(!open)}>
                 <Image src="/plus.png" alt='' width={16} height={16}/>
             </button>
             {open && (
@@ -31,8 +31,9 @@ const Write = () => {
                     </button>
                 </div>
             )}
-            <ReactQuill theme='bubble' value={value} onChange={setValue} placeholder='Tell Your Story'/>
+            <ReactQuill className={styles.textArea} theme='bubble' value={value} onChange={setValue} placeholder='Tell Your Story'/>
         </div>
+        <button className={styles.publish}>Publish</button>
     </div>
   );
 };
